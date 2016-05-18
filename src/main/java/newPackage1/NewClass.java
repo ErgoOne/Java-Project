b2d575f846d16af6e56f27fdc2dee28c1f36b918
margauxthomas <margaux.thomas13@gmail.com>
2016-05-18 15:41:35 +0200

    creer room

--- src/main/java/newPackage1/NewClass.java
+++ src/main/java/newPackage1/NewClass.java
@@ -7,6 +7,7 @@ package newPackage1;
 
 import java.util.HashMap;
 import java.util.Map;
+import static newPackage1.DButilities.auth;
 import static newPackage1.DButilities.AfficherRoom;
 import static newPackage1.DButilities.CreerRoom;
 import static newPackage1.DButilities.getDroitUR;
@@ -20,19 +21,23 @@ public class NewClass {
     public static void main(String[] args) {
         //--
         TypeDroit test;
+        int tmp;
         User u1 = new User("Margx");
         
-        Room r1 = new Room("R1");
-        Room r2 = new Room("R3");
+
+      
         String texte="Salon de css";
+        Room r1 = new Room("R1","blabla");
         //Room r2 = new Room();
         //--
         test=getDroitUR(u1,r1);
         System.out.println("res : "+test);
+
        
         AfficherRoom(); //Requete sql pour afficher les rooms enregistrée dans la bdd
-        CreerRoom(r2,texte,u1);
-        
+        CreerRoom(r1,texte,u1);
+
+     
         /*Map<Room, TypeDroit> du1 = new HashMap<>();
         du1.put(r2, TypeDroit.NONE);
         du1.put(r1, TypeDroit.RW);
