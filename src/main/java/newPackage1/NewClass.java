dc4ad69b65844025a4e528d5551ce8958b0a1b27
Jonathan <Jonathan@PC-D-JOJO>
2016-05-18 15:15:25 +0200

    Connexion avec variable réussie

--- src/main/java/newPackage1/NewClass.java
+++ src/main/java/newPackage1/NewClass.java
@@ -20,7 +20,7 @@ public class NewClass {
         TypeDroit test;
         User u1 = new User("Margx");
         
-        Room r1 = new Room("R1");
+        Room r1 = new Room("R1","blabla");
         //Room r2 = new Room();
         //--
         test=getDroitUR(u1,r1);
